7172ecc96495d3a2c8d03537ecebeb88a0482f45
bjorkoli <bjorkoli@arcada.fi>
2025-04-21 17:25:00 +0300

    Code Challenge 1

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -1,6 +1,7 @@
 package fi.arcada.codechallenge;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView helloText = findViewById(R.id.hello);
+        helloText.setText("Code Challenge");
+
+        TextView minAppText = findViewById(R.id.minApp);
+        minAppText.setText("Min app funkar!");
 
     }
 }
